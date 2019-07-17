2975fce5ff204956c5f999bf7ace5a951ac0d232
ranggawdoes <ranggawdoes@gmail.com>
2019-07-17 15:59:12 +0700

    solved1

--- src/App.js
+++ src/App.js
@@ -25,17 +25,20 @@ function App() {
   return (  
     <Router>
       <div>
+      {/* User Pengunjung */}
+        {/* <Route render={() => <Redirect to={{pathname: '/'}} />} /> */}
+        <Route exact path="/" component={Home} /> 
+        <Route path="/about" component={About} />
+        <Route path="/department" component={Department} />
+        <Route path="/partner" component={Partner} />
+
+      {/* User Student */}
         <Route exact path="/student" component={Login} />
         <Route exact path="/student/upload/:id" component={StudentUpload} />
         <Route exact path="/student/upload-detail" component={StudentUploadDetail} />
         <Route exact path="/student/profile" component={ProfileStudent} />
-        <Route exact path="/" component={Home} /> 
-        <Route path="/about" component={About} />
-        <Route path="/department" component={Department} />
         <Route path="/students" component={Students} />
-        <Route render={() => <Redirect to={{pathname: '/'}} />} />
-        <Route path="/studentProfile/:id" component={StudentProfile} />
-        <Route path="/partner" component={Partner} />
+        <Route path="/studentProfile" component={StudentProfile} />
       </div>
     </Router>
   )
